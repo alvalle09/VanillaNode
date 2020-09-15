18937772762f7bf9075005aee0363c7d7ecfd899
alvalle09 <phredvalle@gmail.com>
2020-09-14 21:28:14 -0500

    use url parse to get full path

--- index.js
+++ index.js
@@ -7,11 +7,23 @@
  */
 
  // Dependencies
- var http = require("http");
+ const http = require("http");
+ const url = require('url');
 
- // The server shold respond to all requests with a string
  var server = http.createServer(function(req, res){
+    // Get the url and parse it
+    const parsedUrl = url.parse(req.url, true);
+    //console.log(parseUrl)
+    // Get the path
+    const path = parsedUrl.pathname;
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+    // Send the response
     res.end('Hola Mundo!!!\n');
+
+   // Log the request path
+   console.log('Request received on path:' + trimmedPath);
+    
  });
 
  // Start the server, and have it listen onport 3000
