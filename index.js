5fe63ad103f063394da9097590a5c6b73fb1a30e
alvalle09 <phredvalle@gmail.com>
2020-09-23 01:19:02 -0500

    capture payload sent by postman

--- index.js
+++ index.js
@@ -1,43 +1,55 @@
-
- /*
+/*
  *
  * Primary file for the API
  *
- * 
+ *
  */
 
- // Dependencies
- const http = require("http");
- const url = require('url');
+// Dependencies
+const http = require("http");
+const url = require("url");
+const StringDecoder = require('string_decoder').StringDecoder;
 
- var server = http.createServer(function(req, res){
-    // Get the url and parse it
-    const parsedUrl = url.parse(req.url, true);
-    //console.log(parseUrl)
+const server = http.createServer(function (req, res) {
+  // Get the url and parse it
+  const parsedUrl = url.parse(req.url, true);
 
-    // Get the path
-    const path = parsedUrl.pathname;
-    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+  // // Get the path
+  const path = parsedUrl.pathname;
+  const trimmedPath = path.replace(/^\/+|\/+$/g, "");
 
-    // Get the query string as an object
-    const queryStringObject = parsedUrl.query;
+  // // Get the query string as an object
+  const queryStringObject = parsedUrl.query;
 
-    // Get the HTTP Method
-    const method = req.method.toLowerCase();
+  // // Get the HTTP Method
+  const method = req.method.toLowerCase();
 
-    // Get headers as an objec
-    const headers = req.headers;
+  // // Get headers as an objec
+  const headers = req.headers;
+
+  // // Get payload, if any
+  const decoder = new StringDecoder("utf-8");
+  var buffer = "";
+
+  req.on("data", function (data) {
+    buffer += decoder.write(data);
+  });
+
+  req.on("end", function () {
+    buffer += decoder.end();
 
     // Send the response
-    res.end('Hola Mundo!!!\n');
+    res.end("Hola Mundo!!!\n");
 
-   // Log the request path ( objects are passed as separate parms in console log)
-   console.log('Request received with headers:', headers);
-   console.log('Request received on path:' + trimmedPath + ' with method:' + method + ' and query parms: ', queryStringObject);
-    
- });
+    // Log the request path ( objects are passed as separate parms in console log)
+    console.log('Request received with headers:', headers);
+    console.log('Request received with payload:', buffer);
+    console.log('Request received on path:' + trimmedPath + ' with method:' + method + ' and query parms: ', queryStringObject);
+  });
 
- // Start the server, and have it listen onport 3000
- server.listen(3000, function() {
-     console.log("The server is listening on port 3000...");
- });
+});
+
+//  // Start the server, and have it listen onport 3000
+server.listen(3000, function () {
+  console.log("The server is listening on port 3000...");
+});
