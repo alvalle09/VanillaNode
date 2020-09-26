23e6c6b43d8abc1e1f43933e88d8f065140c43d6
alvalle09 <phredvalle@gmail.com>
2020-09-26 14:49:44 -0500

    add content-type header info

--- index.js
+++ index.js
@@ -62,6 +62,7 @@ const server = http.createServer(function (req, res) {
       const payloadString = JSON.stringify(payload);
 
       // Send the response
+      res.setHeader('Content-Type', 'application/json');
       res.writeHead(statusCode);
       res.end(payloadString);
 
